96b2bebf5a97ee5a43bb25237da053ef6b4c5e8d
SaifAyman23 <saifayman3021@gmail.com>
2025-09-13 14:58:47 +0300

    Enable backend-frontend connection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,17 +8,12 @@ import { StyledEngineProvider, ThemeProvider } from "@mui/material";
 
 import Router from "@src/routes/Router";
 import { createTheme } from "@src/theme";
-import { configureFakeBackend } from "@src/common/fake-backend";
 import { useLanguage } from "@src/hooks";
 import { useLayoutContext } from "@src/states";
 
 const AppContent = () => {
   const { i18n } = useLanguage();
   const { themeMode } = useLayoutContext();
-  
-  useEffect(() => {
-    configureFakeBackend();
-  }, []);
 
   // Create theme with current language for RTL support and theme mode
   const theme = createTheme(themeMode, i18n.language);
